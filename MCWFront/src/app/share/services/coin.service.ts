9a094783c73209a740784bc6b6de8db9d41b3765
cepebel <cristina.pedraza@babelgroup.com>
2023-04-12 15:17:12 +0200

    Funcionan botones de compra y venta. No actualiza usuario

--- MCWFront/src/app/share/services/coin.service.ts
+++ MCWFront/src/app/share/services/coin.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Icoin } from '../models/coin.model';
 
 const COIN_API = 'http://localhost:3142/api/coins/';
+const USER_API = 'http://localhost:1342/api/users/';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,11 @@ export class CoinService {
     return this.http.get<Icoin[]>(COIN_API+'all') 
   }
   buyCoin(userId: string, coinId: string, amount: number):Observable<string>{
+    
     return this.http.post<string>(COIN_API+'update',{userId:userId, coinId:coinId, amount:amount})
+  }
+  getUserCoins(userId: string):Observable<Icoin[]>{
+    console.log('A por mis monedas!')
+    return this.http.get<Icoin[]>(COIN_API+'get/coins/user/'+userId)
   }
 }
